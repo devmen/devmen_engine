0a2c64d4aa1de8f540b49520f36d08ac343db04c
zaMmer <alexzammer@gmail.com>
2012-06-20 14:57:09 +0300

    Add Hover effect to dropdown menus

--- app/assets/javascripts/admin/application.js
+++ app/assets/javascripts/admin/application.js
@@ -117,8 +117,44 @@ jQuery.fn.emptyFadeOut = function(duration, delay) {
   });
 };
 
+// Add Hover effect to dropdown menus
+function dropdown_hover_add(all) {
+  var menu_selector = all ? '.dropdown-menu' : '.dropdown-hover .dropdown-menu';
+  var toggle_selector = all ? '.dropdown-toggle' : '.dropdown-hover .dropdown-toggle';
+
+  $(document).on('mouseenter', toggle_selector, function() {
+    $(this).siblings(menu_selector).stop(true, true).delay(200).fadeIn(200, function() {
+      $(this).closest('.btn-group').addClass('open');
+    });
+  }).on('mouseleave', toggle_selector, function(event) {
+    var $ddm = $(this).siblings(menu_selector);
+    var $target = $(event.relatedTarget);
+    if (!$ddm.length ||
+      $target[0] != $ddm[0] &&
+      $target.closest(menu_selector)[0] != $ddm[0]
+    )
+      $ddm.stop(true, true).delay(200).fadeOut(200, function() {
+        $(this).closest('.btn-group.open').removeClass('open');
+      });
+  });
+
+  $(document).on('mouseleave', menu_selector, function(event) {
+    var $ddt = $(this).siblings(toggle_selector);
+    var $target = $(event.relatedTarget);
+    if (!$ddt.length ||
+      $target[0] != $ddt[0] &&
+      $target.closest(toggle_selector)[0] != $ddt[0]
+    )
+      $(this).stop(true, true).delay(200).fadeOut(200, function() {
+        $(this).closest('.btn-group.open').removeClass('open');
+      });  
+  });
+};
+
 // Dom ready initializer
 $(function() {
   // Show message block if there is some messages
-  $('#messages:has(:first-child)').show().emptyFadeOut();  
+  $('#messages:has(:first-child)').show().emptyFadeOut();
+
+  dropdown_hover_add(true);  
 });
