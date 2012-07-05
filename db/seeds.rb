b75800d8ee6cff66a8b9643b0c98492a672a86bc
zaMmer <alexzammer@gmail.com>
2012-07-05 12:19:31 +0300

    Sample data for seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create :name => 'admin', :password => 'admin', :email => 'admin@devmen.ru', :roles => ['admin']
+
+if ['development', 'test'].include? Rails.env
+  # Create some pages
+  Page.destroy_all
+  5.times do |i|
+    attrs = {
+      :name => Faker::Lorem.words(rand(2..3)).join(' ').capitalize,      
+      :body => "<h1>#{Faker::Lorem.sentence}</h1><p>#{Faker::Lorem.paragraphs(rand(3..10)).join(' ')}</p>"
+    }
+    attrs[:url] = Faker::Lorem.words(rand(2..3)).join('-').downcase if i.odd?
+    Page.create(attrs)
+  end
+
+  # Create some news
+  if News.present?
+    News::Entry.destroy_all
+    5.times do |i|
+      attrs = {
+        :name => Faker::Lorem.words(rand(2..3)).join(' ').capitalize,
+        :date => Time.new - rand(0..30).days,
+        :text => "<h1>#{Faker::Lorem.sentence}</h1><p>#{Faker::Lorem.paragraphs(rand(3..10)).join(' ')}</p>"
+      }
+      News::Entry.create(attrs)
+    end
+  end
+end
