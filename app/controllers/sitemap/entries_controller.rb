6a20fc168cd0b42e5c7ececc0c2c2a2d61bd7dfc
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2012-10-17 12:08:21 +0300

    sitemap menu, add categories and products to sitemap

--- app/controllers/sitemap/entries_controller.rb
+++ app/controllers/sitemap/entries_controller.rb
@@ -1,7 +1,13 @@
 class Sitemap::EntriesController < ApplicationController
+  caches_action :index, expires_in: 4.hours
+
   def index
     @pages = Page.all
-    @news = News::Entry.all if News.present?
-    @realties = Realty::Entry.all if Realty.present?
+    @news_categories = News::Category.order(:name) if News.present?
+    @realty_categories = Realty::Category.order(:name) if Realty.present?
+    if Shop.present?
+      @product_categories = Shop::Category.order(:name)
+      @products = Shop::Product.includes(:pictures)
+    end
   end
 end
